60c0e8b25fc319c14aa56a4f242c76f1b020b5fa
Naeli <naelisilva15@gmail.com>
2020-04-27 23:31:09 -0300

    arrumando

--- src/main/java/algaworks/primeirojava/controller/clientecontroller.java
+++ src/main/java/algaworks/primeirojava/controller/clientecontroller.java
@@ -1,25 +1,42 @@
 package algaworks.primeirojava.controller;
 
-import java.util.Arrays;
-import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import algaworks.primeirojava.domain.repository.ClienteRepository;
 import algaworks.primeirojava.models.Cliente;
 
 @RestController
+@RequestMapping("/clientes")
 public class clientecontroller {
 
     @Autowired
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar() {
         // return clienteRepository.findAll();
         return clienteRepository.findByNomeContaining("Silva");
     }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity <Cliente> buscar(@PathVariable final Long clienteId) {
+        final Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+    return clienteRepository.save(cliente);
+}
 }

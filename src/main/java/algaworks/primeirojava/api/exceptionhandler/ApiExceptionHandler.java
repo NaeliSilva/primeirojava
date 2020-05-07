15686407c740fc1b776386b30d11e2648254f397
Naeli <naelisilva15@gmail.com>
2020-05-07 12:04:49 -0300

    Include

--- src/main/java/algaworks/primeirojava/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/algaworks/primeirojava/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,8 +13,12 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import algaworks.primeirojava.controller.NegocioException;
+import net.bytebuddy.asm.Advice.Return;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -22,6 +26,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @Autowired
     private MessageSource messageSource; 
 
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+        var status = HttpStatus.BAD_REQUEST;
+
+        var problema = new Problema();
+        problema.setStatus(status.value());
+        problema.setTitulo(ex.getMessage());
+        problema.setDataHora(LocalDateTime.now());
+
+        return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+
+    }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
--- src/main/java/algaworks/primeirojava/api/exceptionhandler/Problema.java
+++ src/main/java/algaworks/primeirojava/api/exceptionhandler/Problema.java
@@ -3,6 +3,11 @@ package algaworks.primeirojava.api.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+
+@JsonInclude (Include.NON_NULL)
 public class Problema {
 
     private Integer status;

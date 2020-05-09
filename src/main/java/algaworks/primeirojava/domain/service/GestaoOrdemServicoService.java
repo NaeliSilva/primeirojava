f8869c135dac70774399ea5824dde23a1d5bf760
Naeli <naelisilva15@gmail.com>
2020-05-09 20:01:31 -0300

    cliente nao encontrado

--- src/main/java/algaworks/primeirojava/domain/service/GestaoOrdemServicoService.java
+++ src/main/java/algaworks/primeirojava/domain/service/GestaoOrdemServicoService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import algaworks.primeirojava.domain.repository.OrdemServicoRepository;
+import algaworks.primeirojava.controller.NegocioException;
 import algaworks.primeirojava.domain.repository.*;
+import algaworks.primeirojava.models.Cliente;
 import algaworks.primeirojava.models.OrdemServico;
 import algaworks.primeirojava.models.StatusOrdemServico;
 
@@ -16,7 +18,13 @@ public class GestaoOrdemServicoService {
     @Autowired
     private OrdemServicoRepository ordemServicoRepository;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     public OrdemServico criar(OrdemServico ordemServico) {
+        Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId()).orElseThrow(() -> new NegocioException("Cliente não encontrado"));
+
+        ordemServico.setCliente(cliente);
         ordemServico.setStatus(StatusOrdemServico.ABERTA);
         ordemServico.setDataAbertura(LocalDateTime.now());
 

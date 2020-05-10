80aced9375aa8e8873fbdcd217c4d49bb6fe9b03
Naeli <naelisilva15@gmail.com>
2020-05-10 17:40:16 -0300

    se encontrou retorna ok se nao 404

--- src/main/java/algaworks/primeirojava/controller/OrdemServicoController.java
+++ src/main/java/algaworks/primeirojava/controller/OrdemServicoController.java
@@ -1,10 +1,16 @@
 package algaworks.primeirojava.controller;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
+
+import com.jayway.jsonpath.Option;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +42,19 @@ public class OrdemServicoController {
     public java.util.List<OrdemServico> Listar() {
         return ordemServicoRepository.findAll();
     }
+
+    @GetMapping("/{ordemServicoId}")
+    public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemServicoId) {
+      
+        Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
+
+        if (ordemServico.isPresent()){
+            return ResponseEntity.ok (ordemServico.get());
+        }
+
+        return ResponseEntity.notFound().build();
+
+    }
+
 
 }
